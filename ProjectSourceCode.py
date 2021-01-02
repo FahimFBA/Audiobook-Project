c744ea6ccfb34205501ff608b6377046bb5d323d
FahimFBA <fahimbinamin@gmail.com>
2021-01-02 16:55:17 +0600

    Included documentation

--- ProjectSourceCode.py
+++ ProjectSourceCode.py
@@ -1,26 +1,45 @@
-from tkinter import *
+# Python audiobook project
+
+# This project is done by: Md. Fahim Bin Amin
+
+from tkinter import *   # Importing the GUI named tkinter
+
+
 def close_window():
-    root.destroy()
+    root.destroy()  # Destroying the main window
+
+ # Creating the tkinter window 
+
+
 root = Tk()
 frame = Frame(root)
 frame.pack()
 bottomframe = Frame(root)
 bottomframe.pack( side = BOTTOM )
-redbutton = Button(frame, text="Browse and select the PDF file you want to process.", fg="blue", command=close_window)
+redbutton = Button(frame, text="Browse and select the PDF file you want to process.", fg="blue", command=close_window)  # The window will get closed by the command
 redbutton.pack( side = LEFT)
+
+
+# Adding widget to the root window
+
 Label(root, text='FBA Project: Audiobook', font=('Comic Sans MS', 25)).pack(side=TOP, pady=10)
-photo = PhotoImage(file=r"C:\\Users\\FBA Desktop\\Downloads\\AudiobookGUICanvas.gif")
-Button(root, image=photo).pack(side=TOP)
+# other stylish fonts can be used here too
+
+photo = PhotoImage(file=r"C:\\Users\\FBA Desktop\\Downloads\\AudiobookGUICanvas.gif")  # Creating a photoimage object to use image . JPEG wouldn't work here
+Button(root, image=photo).pack(side=TOP)  # Setting image in button
 root.mainloop()
+
+# Executing the command for text to speech
+
 import pyttsx3
 import PyPDF2
-engine = pyttsx3.init()
+engine = pyttsx3.init()  # Object creation
 rate = engine.getProperty('rate')
-print (rate)
-engine.setProperty('rate', 185)
+print (rate)  # Printing the current voice rate
+engine.setProperty('rate', 185)   # Setting up the new voice rate
 volume = engine.getProperty('volume')
-print (volume)
-engine.setProperty('volume',1.0)
+print (volume)  # Printing the current volume level
+engine.setProperty('volume',1.0)  # Setting up the volume level between 0 and 1
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 from tkinter.filedialog import *
@@ -33,6 +52,6 @@ for num in range(0,pages):
     player=pyttsx3.init()
     player.say(text)
     player.runAndWait()
-engine.save_to_file(text, 'Audiobook created by FBA.mp3')
+engine.save_to_file(text, 'Audiobook created by FBA.mp3')   # Saving the voice to a file 
 engine.runAndWait()
 print("Your audiobook file has been generated as an mp3 file. Check the project file directory for getting the file.")
